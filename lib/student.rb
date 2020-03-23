4149d1847add96803ec377fbb874536bae0d5f8f
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-23 14:56:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_reader :name, :grade
+  attr_reader :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
 
 
 end
